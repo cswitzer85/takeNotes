b3cbfff86e91206c28f6dfa08722487e5f394cf9
Cody Switzer <cswitzer85@gmail.com>
2020-02-21 12:16:06 -0600

    setup html routes

---
+++ server.js
@@ -0,0 +1,32 @@
+// Dependencies
+// =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+var express = require("express")
+var path = require("path")
+// Express setup
+// =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+var app = express();
+var PORT = process.env.PORT || 9001;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Variables
+// =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+// var notes = [];
+
+// Routes
+// =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+	res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// Server listening
+// =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+app.listen(PORT, function () {
+	console.log("App listening on PORT " + PORT);
+});
